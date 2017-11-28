869d2b3e0f48f9c5cdc910fae9939efdc25f0e78
xinghunm <xinghunm@curacloudcorp.com>
2017-11-28 16:01:15 +0800

    revise example6/7

--- src/views/example6/example6.jsx
+++ src/views/example6/example6.jsx
@@ -18,6 +18,51 @@ Hello.propTypes = {
 };
 Hello.defaultProps = {
   place: "world",
+  // optionalArray: PropTypes.array,
+  // optionalBool: PropTypes.bool,
+  // optionalFunc: PropTypes.func,
+  // optionalNumber: PropTypes.number,
+  // optionalObject: PropTypes.object,
+  // optionalString: PropTypes.string,
+  // optionalSymbol: PropTypes.symbol,
+  // optionalNode: PropTypes.node,
+
+  // // A React element.
+  // optionalElement: PropTypes.element,
+
+  // You can also declare that a prop is an instance of a class. This uses
+  // JS's instanceof operator.
+  // optionalMessage: PropTypes.instanceOf(Message),
+
+  // You can ensure that your prop is limited to specific values by treating
+  // it as an enum.
+  // optionalEnum: PropTypes.oneOf(['News', 'Photos']),
+
+  // An object that could be one of many types
+  // optionalUnion: PropTypes.oneOfType([
+  //   PropTypes.string,
+  //   PropTypes.number,
+  //   PropTypes.instanceOf(Message)
+  // ]),
+
+  // // An array of a certain type
+  //  optionalArrayOf: PropTypes.arrayOf(PropTypes.number),
+  //
+  //  // An object with property values of a certain type
+  //  optionalObjectOf: PropTypes.objectOf(PropTypes.number),
+  //
+  //  // An object taking on a particular shape
+  //  optionalObjectWithShape: PropTypes.shape({
+  //    color: PropTypes.string,
+  //    fontSize: PropTypes.number
+  //  }),
+  //
+  //  // You can chain any of the above with `isRequired` to make sure a warning
+  //  // is shown if the prop isn't provided.
+  //  requiredFunc: PropTypes.func.isRequired,
+  //
+  //  // A value of any data type
+  //  requiredAny: PropTypes.any.isRequired,
 };
 /*
 1.组件的属性可以是任意值，有时需要一种机制来判定约束所提供的属性是否符合要求
--- src/views/example7/example7.jsx
+++ src/views/example7/example7.jsx
@@ -9,15 +9,29 @@ import React, { PropTypes } from 'react';
   的参数则为该组件的具体实例。组件被卸载或者原有的ref属性本身发生变化时，回调函数也会立即执行，此时
   回调函数的参数为null,以防止内存泄漏。
 */
-class TextField extends React.Component {
+class Son extends React.Component {
   handleClick() {
     this.myTextField.focus();
   }
   render() {
     return (
       <div>
-        <input type="text" ref={(e) => { this.myTextField = e; }} />
+        <input type="text" ref={this.props.inputRef} />
         <button onClick={this.handleClick.bind(this)} />
+      </div>
+    );
+  }
+}
+
+class Parent extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* 使父类可以访问子类, 此例中父类可通过this.inputElement来访问子类的input */}
+        <Son inputRef={(e) => {
+          this.inputElement = e;
+          console.log(this.inputElement);
+        }} />
       </div>
     );
   }
@@ -25,7 +39,7 @@ class TextField extends React.Component {
 
 const createExample7 = () => {
   ReactDOM.render(
-    <TextField />
+    <Parent />
     , document.getElementById('app')
   );
 };
