57b54b84e4705ebb1fc158cc59d32a7941b60c72
xinghunm <xinghunm@curacloudcorp.com>
2017-11-28 10:46:36 +0800

    add key to array

--- src/views/example3/example3.jsx
+++ src/views/example3/example3.jsx
@@ -2,7 +2,8 @@ import ReactDOM from 'react-dom';
 import React from 'react';// jsx编译需要调用React.createElement，因此React需在作用域内
 
 
-const namesArr = [<h1>jay</h1>,<h1>xh</h1>,<h1>snow</h1>];
+const namesArr = [<h1>jay</h1>,<h1>xh</h1>,<h1>snow</h1>];// eslint-disableline
+const foods = ["meat", "fish", "apple"];
 /*
   jsx允许插入变量，如果变量是一个数组则会展开这个数组的所有成员。
  */
@@ -10,6 +11,8 @@ const createExample3 = () => {
   ReactDOM.render(
     <div>
       { namesArr }
+      {foods.map((food) =>
+        <span key={food}>food</span>)} 
     </div>
     , document.getElementById('app')
   );
